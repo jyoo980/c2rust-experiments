c5d2587671e4aa6f88a3a80791c47d38affdfaf8
James Yoo <jyoo980@gmail.com>
2025-10-05 16:22:38 -0700

    Updating `check_requirements.py`

--- check_requirements.py
+++ check_requirements.py
@@ -10,12 +10,9 @@ MINIMUM_RUST_VERSION = Version.parse("1.58.0")
 MINIMUM_KANI_VERSION = Version.parse("0.65.0")
 
 
-def main():
-    print(
-        f"Checking requirements for:\nCBMC == {CBMC_VERSION}\nRust >= {MINIMUM_RUST_VERSION}\nKani >= {MINIMUM_KANI_VERSION}"
-    )
+def main() -> None:
     assert _is_cbmc_installed() and _is_rust_installed() and _is_kani_installed()
-    print("Requirements satisfied.")
+    print("All requirements satisfied")
 
 
 def _is_cbmc_installed() -> bool:
@@ -23,6 +20,7 @@ def _is_cbmc_installed() -> bool:
     result = _run_cmd(get_cbmc_version)
     installed_cbmc_version = Version.parse(result.stdout.split(" ")[0])
     if installed_cbmc_version.compare(CBMC_VERSION) == 0:
+        print(f"Requirement CBMC == {CBMC_VERSION} is satisfied")
         return True
     raise Exception(
         f"CBMC version {CBMC_VERSION} is required, but found {installed_cbmc_version}"
@@ -35,6 +33,7 @@ def _is_rust_installed() -> bool:
     rust_version_str = result.stdout.split(" ")[1]
     installed_rust_version = Version.parse(rust_version_str)
     if installed_rust_version.compare(MINIMUM_RUST_VERSION) >= 0:
+        print(f"Requirement Rust >= {MINIMUM_RUST_VERSION} is satisfied")
         return True
     raise Exception(
         f"Expected Rust version >={MINIMUM_RUST_VERSION}, but found {installed_rust_version}"
@@ -47,13 +46,14 @@ def _is_kani_installed() -> bool:
     kani_version_str = result.stdout.split(" ")[1]
     installed_kani_version = Version.parse(kani_version_str)
     if installed_kani_version.compare(MINIMUM_KANI_VERSION) >= 0:
+        print(f"Requirement Kani >= {MINIMUM_KANI_VERSION} is satisfied")
         return True
     raise Exception(
         f"Expected Kani version >={MINIMUM_RUST_VERSION}, but found {installed_kani_version}"
     )
 
 
-def _run_cmd(cmd: str) -> CompletedProcess:
+def _run_cmd(cmd: str) -> CompletedProcess[str]:
     try:
         return subprocess.run(
             args=cmd, shell=True, check=True, capture_output=True, encoding="utf-8"
